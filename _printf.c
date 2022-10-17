9aeca788df5ac81f6cd3d0b9f4a13596b20fea47
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-17 01:23:33 -0700

    new commit

--- _printf.c
+++ _printf.c
@@ -34,7 +34,7 @@ int _printf(const char *format, ...)
 					handl_buf(buffer, format[i], ibuf), len++, i--;
 				}
 				else
-				{	
+				{
 					len += function(arguments, buffer, ibuf);
 					i += ev_print_func(format, i + 1);
 				}
