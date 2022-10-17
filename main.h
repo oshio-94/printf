ce1f52f0c767573b0b6d1a5e6569197e63e58ac1
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-17 00:54:00 -0700

    main header file

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - struct for printing various 
+ * @type_arg: identifiers / which type to print
+ * @f: function to print
+ *
+ * Description: struct that stores pointers
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+
+
+#endif
