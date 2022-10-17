fba7e625a013013ec52adcbbd631311599877d3d
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-17 02:30:26 -0700

    printf corrections

--- print_buff.c
+++ print_buff.c
@@ -5,7 +5,7 @@
  * @nbuff: size of buffers
  * Return: returns the size of buffers
  */
-int print_buf(char *buf, unsigned int nbuff)
+int print_buff(char *buf, unsigned int nbuff)
 {
 	return (write(1, buf, nbuff));
 }
